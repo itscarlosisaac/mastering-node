feecba1b1539a55f7ef8817be5fc4067b5718d86
Carlos Martinez <itscarlosisaac@gmail.com>
2018-11-16 11:03:51 -0400

    Basic auth with redis and express.

---
+++ Chapter 5/auth.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const redis = require('redis');
+
+const client = redis.createClient();
+
+http.createServer((req, res) => {
+  let auth = req.headers['authorization'];
+  if (!auth ) {
+    res.writeHead(401, {'WWW-Authenticate': 'Basic realm="Secure Area"'});
+    return res.end('<html><body>Please enter some credentials.</body></html>')
+  }
+  let tmp = auth.split(' ');
+  let buf = Buffer.from(tmp[1], 'base64');
+  let plain_auth = buf.toString();
+  let creds = plain_auth.split(':');
+  let username = creds[0];
+  // Find the user in the record;
+
+  client.get(username, function(err, data) {
+    if ( err || !data) {
+      res.writeHead(401, {'WWW-Authenticate': 'Basic realm="Secure Area"'});
+      return res.end('<html><body>You are not authorized to view this area.</body></html>');
+    }
+    res.statusCode = 200;
+    return res.end('<html><body>Welcome!</body></html>')
+  })
+}).listen(8080);
---
+++ Chapter 5/login.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto')
+const fs = require('fs')
+const express = require('express')
+const redis = require('redis')
+
+let app = express();
+let client = redis.createClient();
+
+app.get('/authenticate/:username', (request, response) => {
+  let publicKey = Math.random();
+  let username = request.params.username;
+
+  client.hgetall(username, (err, data) => {
+    if( err || !data) { return response.end("no data"); }
+    let challenge = crypto.createHash('sha256').update(publicKey + data.passsword ).digest('hex');
+    client.set(challenge, username);
+    response.end(challenge);
+  });
+});
+
+app.get('/login/:response', (request, response) => {
+  let challengeHash = request.params.respose;
+  client.exists(challengeHash, (err, exists) => {
+    if( err || !exists ){ return response.end("Failed"); }
+    client.del(challengeHash, () => response.end("OK"));
+  });
+});
+
+app.get('/', (request, response) => {
+  fs.readFile('./auth.html', (err, data) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(data);
+  });
+});
+
+app.get('/256', (request, response) => {
+  fs.readFile('./sha256.js', (err, data) => {
+      response.end(data);
+  });
+});
+
+
+app.listen(8080);
